88b51e06b6f1b1c63c1a913fd424dc8b2f38410e
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-28 09:47:30 +0200

    add random

--- Basic/main.js
+++ Basic/main.js
@@ -4,6 +4,21 @@
 
 // ex 1 | Temperature v1.0
 
-let randomNum = Math.floor(Math.random() * 11);
+
+
+
+
+
+let randomNum = Math.floor(Math.random() * 30) - 5;
 
 console.log(randomNum);
+
+
+
+
+
+
+
+// function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
