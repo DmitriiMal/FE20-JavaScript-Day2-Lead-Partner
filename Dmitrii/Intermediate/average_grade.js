0a925e38fc21354acff8c9ee48ed92e999ea457b
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-28 12:20:38 +0200

    complete Average Grade

---
+++ Dmitrii/Intermediate/average_grade.js
@@ -0,0 +1,9 @@
+function averageGrade(math, physics, english) {
+  let sum = math + physics + english;
+  let average = sum / 3;
+  console.log(sum);
+  console.log(average);
+
+}
+
+averageGrade(3, 4, 5);
