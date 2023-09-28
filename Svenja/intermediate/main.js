e90b4e39fab2c6a764c20d5307b5f615ef282c6f
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-09-28 13:23:11 +0200

    converting values into numbers

--- Svenja/intermediate/main.js
+++ Svenja/intermediate/main.js
@@ -2,6 +2,7 @@
 let message = "i am a web developer";
 
 const capitalize = (function () {
+    message = String(message);
     let firstLetter = message.charAt(0).toUpperCase();
     return firstLetter + message.substring(1);
 }())
@@ -11,10 +12,13 @@ console.log(capitalize);
 
 // AVERAGE GRADE
 function average(math, physics, english) {
+    math = Number(math);
+    physics = Number(physics);
+    english = Number(english);
     sum = math + physics + english;
     average = sum / 3;
     result = `<strong>Sum: ${sum}<br><br>Average: ${average}</strong>`
     document.write(result);
 }
 
-average(1, 2, 3);
+average("1", 2, 3);
