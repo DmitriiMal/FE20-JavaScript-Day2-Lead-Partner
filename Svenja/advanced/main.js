9787cbace6d396ba27197cac941c3f28808c6f53
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-09-28 14:42:30 +0200

    finished the challenge

--- Svenja/advanced/main.js
+++ Svenja/advanced/main.js
@@ -1,10 +1,21 @@
 // TIME CONVERT
+
+// function timeConverter() {
+//     let input = prompt("Please write a number of minutes.");
+//     let hours = Math.floor(input / 60);
+//     let minutes = input % 60;
+//     let message = `${input} minutes = ${hours} hours and ${minutes} minute(s).`;
+//     return message;
+// }
+
+// console.log(timeConverter());
+
+
 function timeConverter() {
     let input = prompt("Please write a number of minutes.");
     let hours = Math.floor(input / 60);
     let minutes = input % 60;
-    let message = `${input} minutes = ${hours} hours and ${minutes} minute(s).`;
-    return message;
+    return `${input} minutes = ${hours} hours and ${minutes} minute(s).`;
 }
 
 console.log(timeConverter());
