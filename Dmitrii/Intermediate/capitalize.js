ce7df3ce5621cacc547bc5acf3ead7268d4bb85d
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-28 13:17:05 +0200

    fix capitalize

--- Dmitrii/Intermediate/capitalize.js
+++ Dmitrii/Intermediate/capitalize.js
@@ -1,7 +1,26 @@
 
-function firstUpp(message) {
-  let firstLettoUp = message.charAt(0).toUpperCase();
-  console.log(firstLettoUp + message.substring(1));
-};
+let message = "i am a web developer";
 
-firstUpp('tea');
+const firstUpp = (function () {
+  let firstLettoUp = message.charAt(0).toUpperCase();
+  return firstLettoUp + message.substring(1);
+}());
+
+
+console.log(firstUpp)
+
+
+
+
+
+
+
+
+
+
+// function firstUpp(message) {
+//   let firstLettoUp = message.charAt(0).toUpperCase();
+//   console.log(firstLettoUp + message.substring(1));
+// };
+
+// firstUpp('tea');
